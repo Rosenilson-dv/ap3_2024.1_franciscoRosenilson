b79fede67cb6aa05c0d2386b585d281db6083a3b
Francisco Rosenilson <franciscorosenilson10@gmail.com>
2024-07-25 00:03:04 -0300

    Update OlaMundo.java

--- ap3_franciscoRosenilson/ap3_franciscoRosenilson/src/OlaMundo.java
+++ ap3_franciscoRosenilson/ap3_franciscoRosenilson/src/OlaMundo.java
@@ -3,3 +3,9 @@ public class OlaMundo {
         System.out.println("Olá, mundo");
     }
 }
+
+public class Aluno {
+    public static void main(String[] args) throws Exception {
+        System.out.println(x: "Nome")
+    }
+}
