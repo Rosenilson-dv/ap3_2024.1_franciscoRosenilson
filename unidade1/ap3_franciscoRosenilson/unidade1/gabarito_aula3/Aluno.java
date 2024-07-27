18f24ef32b15c47a9babbea6b02f37c3f0eb3094
Francisco Rosenilson <franciscorosenilson10@gmail.com>
2024-07-27 00:26:03 -0300

    aula 3 gabarito corrigido

--- unidade1/ap3_franciscoRosenilson/unidade1/gabarito_aula3/Aluno.java
+++ unidade1/ap3_franciscoRosenilson/unidade1/gabarito_aula3/Aluno.java
@@ -2,21 +2,18 @@ package ap3_franciscoRosenilson.unidade1.gabarito_aula3;
 
 public class Aluno {
 
-    String nomeDoAluno = "SemNome";
-    double nota1 = -1;
-    double nota2 = -1;
-    double nota3 = -1;
+    String nome;
+    double nota1;
+    double nota2;
+    double nota3;
     
-    public String getNomeDoAluno() {
-        return nomeDoAluno;
-    }
 
     public String getNomeMaisculo() {
-        return nomeDoAluno.toUpperCase();
+        return nome.toUpperCase();
     }
 
     public String getNomeMinusculo() {
-        return nomeDoAluno.toLowerCase();
+        return nome.toLowerCase();
     }
 
     public double getMedia() {
@@ -33,8 +30,8 @@ public class Aluno {
         return nome;
     }
 
-    public void setNomeDoAluno(String nomeDoAluno) {
-        this.nomeDoAluno = nomeDoAluno;
+    public void setNome(String nome) {
+        this.nome = nome;
     }
     public double getNota1() {
         return nota1;
@@ -54,4 +51,9 @@ public class Aluno {
     public void setNota3(double nota3) {
         this.nota3 = nota3;
     }
+    @Override
+    public String toString() {
+        return "Aluno [nome=" + nome + ", nota1=" + nota1 + ", nota2=" + nota2 + ", nota3=" + nota3 + "]";
+
     }
+}
